fa32b79a27145f55b651196f16b7b6834c31d1f1
Fabrone BITITI <40841286+Hol7@users.noreply.github.com>
2021-09-05 04:19:58 +0000

    update
    f - string Function

--- Day2.py
+++ Day2.py
@@ -1,1 +1,22 @@
+# Data type
 
+#String str
+
+# converting Data type   on BMI body max index
+
+height = input("Entree votre Taille: ")
+weight = input("Entree Votre Poids: ")
+
+bmi = int(weight) / float(height) ** 2
+
+bmi_as_number = int(bmi)
+
+print(bmi_as_number)
+
+
+score = 10
+height = 1.8
+is_winning = True
+
+
+print( f" Your Scrore is {score} and your height is {height}")
